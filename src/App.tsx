b8da3ade3b1a47ec9f7503ad132d6186620b012a
LoboAnimae <andrese.quanl@gmail.com>
2024-04-15 20:50:08 -0600

    feature: Adding initial UI
    Adding the initial UI based on MUI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,31 @@
+import { Menu } from "@mui/icons-material";
+import { AppBar, Box, Drawer, IconButton, Toolbar, Typography } from "@mui/material";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "white",
+          boxShadow: "none",
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Menu sx={{ color: "#33363F" }} />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "#33363F" }}>
+            FS TAGGER
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={}></Drawer>
+    </Box>
   );
 }
 
